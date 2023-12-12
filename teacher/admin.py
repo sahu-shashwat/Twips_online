a686b90714cade6ea261898e411249be86e339be
Vnki-Krishna <venkykanusu2424@gmail.com>
2023-12-12 12:55:16 +0530

    Twips

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from teacher.models import teacher_model
+from teacher.models import teacher_model,course_model,domain_model
 # Register your models here.
 
 class teacher_admin(admin.ModelAdmin):
     list_display=['username','first_name','last_name','email','teacher_id','phone_number','year_of_experience','gender','password']
-admin.site.register(teacher_model,teacher_admin)    
+admin.site.register(teacher_model,teacher_admin) 
+
+class domain_admin(admin.ModelAdmin):
+    list_display=['did','domain_name','desc']
+admin.site.register(domain_model,domain_admin)
+
+class course_admin(admin.ModelAdmin):
+    list_display=['cid','course_name','desc','image']
+admin.site.register(course_model,course_admin)
